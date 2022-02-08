6d74f5390b6700af7c1e10c92aefb0be7b18e90d
dcampbellcreative <dougcampbell1990@gmail.com>
2022-02-07 20:41:04 -0500

    initial commit, add file for services

---
+++ src/app/fetch-api-data.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@angular/core';
+// import { catchError } from 'rxjs/internal/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, catchError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const apiUrl = 'https://dcampbellcreative-movie-api.herokuapp.com/';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FetchApiDataService {
+
+  constructor(private http: HttpClient) { }
+
+  public userRegistration(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http.post(apiUrl + 'users', userDetails).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  public Login(userDetails: any): Observable<any> {
+    console.log(userDetails);
+    return this.http.post(apiUrl + 'login', userDetails).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private extractResponseData(res: Response): any {
+    const body = res;
+    return body || {};
+  }
+
+  // get list of all movies 
+  getAllMovies(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'movies', {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+  // get movie by title
+  getMovie(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http.get(apiUrl + 'movies/:Title', {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer' + token,
+      })
+    }).pipe(
+      map(this.extractResponseData),
+      catchError(this.handleError)
+    );
+  }
+
+
+
+  private handleError(error: HttpErrorResponse): any {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Some error ocurred:', error.error.message);
+    } else {
+      console.error(
+        `Error Status code ${error.status}, ` +
+        `Error body is: ${error.error}`
+      );
+    }
+    return throwError(
+      'Something bad happened; please try again later'
+    );
+  }
+}
+
